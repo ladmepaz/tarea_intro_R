83507c378227bad777db4e0e53cb624f2328be5b
JoseFRuiz <jfruizmu@unal.edu.co>
2024-10-20 18:14:52 -0500

    Create ejercicio05.R

---
+++ ejercicio05.R
@@ -0,0 +1,10 @@
+# Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
+
+solucion <- function() {
+  # Ordena las asignaciones de manera que el que se devuelva un valor de 's' diferente a NULL
+  a <- 10 # asigna valor aquí
+  b <- 20 # asigna valor aquí
+  s <- a+b
+  s <- NULL
+  return(s)  # Replace with your implementation
+}
