f4894fb5f8ff449daf39cf0bf4bf60500b15ff8b
JoseFRuiz <jfruizmu@unal.edu.co>
2024-10-20 17:59:46 -0500

    Create ejercicio03.R

---
+++ ejercicio03.R
@@ -0,0 +1,9 @@
+# Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
+
+solucion <- function() {
+  # Asigna un valor a 'a' de manera que el valor asignado a 's' sea 5
+  a <- 20 # asigna valor aquí
+  b <- # asigna valor aquí
+  s <- a-b
+  return(s)  # Replace with your implementation
+}
