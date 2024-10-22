91614ef37f4dc4ef46ec369ca25cfb9ed933b52f
JoseFRuiz <jfruizmu@unal.edu.co>
2024-10-22 14:28:29 -0500

    Update ejercicio05.R

--- ejercicio05.R
+++ ejercicio05.R
@@ -1,7 +1,7 @@
 # Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
 
 solucion <- function() {
-  # Ordena las asignaciones de manera que el que se devuelva un valor de 's' diferente a NULL
+  # Ordena (sin realizar nuevas asignaciones) de manera que se devuelva un valor de 's' diferente a NULL
   a <- 10
   b <- 20
   s <- a+b
